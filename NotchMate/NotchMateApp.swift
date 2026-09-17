b2251c99bf27b0812bba9ee3c2caf092aebeb85b
Tanishq Sharma <tanishqsharma.sharma@gmail.com>
2026-09-17 14:37:16 +0530

    Initial Commit

---
+++ NotchMate/NotchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchMateApp.swift
+//  NotchMate
+//
+//  Created by Tanishq Sharma on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
